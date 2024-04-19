e5aae5c849ef0583640a32e6f58c476b7c5ceb7c
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-04-19 15:43:59 -0600

    added header file with links

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import Nav from './components/Nav';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <>
-      <Nav />
+      <Header />
       <main>
         <Outlet />
       </main>
---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+import Navbar from './UI/Navbar';
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Itzel Hernandez</h1>
+            <Navbar
+                links ={[
+                    <Link key={1} to="/">
+                        About Me
+                    </Link>,
+                    <Link key={2} to="/portfolio">
+                        Portfolio
+                    </Link>,
+                    <Link key={3} to="/contact">
+                        Contact
+                    </Link>,
+                    <Link key={4} to="/resume">
+                        Resume
+                    </Link>
+                ]}
+            />
+        </header>
+    )
+}
