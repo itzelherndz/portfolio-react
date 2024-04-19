5d071b39cbefdb15cf26b62dc6efeb505852127d
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-04-19 16:08:36 -0600

    added router to main file

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+
+import App from './App';
+import ErrorPage from './pages/ErrorPage';
+import AboutPage from './pages/AboutPage';
+import PortfolioPage from './pages/PortfolioPage';
+import ContactPage from './pages/ContactPage';
+import ResumePage from './pages/ResumePage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <AboutPage />
+      },
+      {
+        path: '/portfolio',
+        element: <PortfolioPage />
+      },
+      {
+        path: '/contact',
+        element: <ContactPage />
+      },
+      {
+        path: '/resume',
+        element: <ResumePage />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
