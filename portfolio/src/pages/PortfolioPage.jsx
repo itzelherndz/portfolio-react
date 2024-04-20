ede4eb60f3552e01195c9f17d7ad4d58e2d8263d
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-04-19 21:07:30 -0600

    added portfolio page

---
+++ portfolio/src/pages/PortfolioPage.jsx
@@ -0,0 +1,12 @@
+import WorkIcon from '../components/WorkIcon';
+
+export default function PortfolioPage() {
+    return (
+        <div>
+            <h2>Portfolio</h2>
+            <div>
+                <WorkIcon image="" title="" github="" deployed=""/>
+            </div>
+        </div>     
+    );
+}
