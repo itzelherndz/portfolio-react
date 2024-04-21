87c2b9392fa6212751dc8b027f517c574496f2f0
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-04-21 14:15:00 -0600

    added work icon component

---
+++ portfolio/src/components/UI/WorkIcon.jsx
@@ -0,0 +1,11 @@
+export default function WorkIcon(image, title, description, github, deployed) {
+    return (
+        <div>
+            <img src={image} alt={title}/>
+            <h3>{title}</h3>
+            <p>{description}</p>
+            <a href={github}>GitHub</a>
+            <a href={deployed}>Application</a>
+        </div>
+    );
+}
