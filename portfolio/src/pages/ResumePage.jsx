a17ad4be98eda3f7e00a86e94aabd43a122b1efe
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-04-22 17:10:05 -0600

    added resume page

---
+++ portfolio/src/pages/ResumePage.jsx
@@ -0,0 +1,33 @@
+export default function ResumePage() {
+    return (
+        <div>
+            <div>
+                <h2>Resume</h2>
+                <a href="">Download</a>
+            </div>
+            <div>
+                <h2>Front-End Proficiencies</h2>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>jQuery</li>
+                    <li>Responsive Design</li>
+                    <li>React</li>
+                    <li>Bootstrap</li>
+                </ul>
+            </div>
+            <div>
+                <h2>Back-End Proficiencies</h2>
+                <ul>
+                    <li>APIs</li>
+                    <li>Node</li>
+                    <li>Express</li>
+                    <li>MySQL/Sequelize</li>
+                    <li>MongoDB/Mongoose</li>
+                    <li>REST</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
