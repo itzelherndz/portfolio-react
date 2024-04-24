fcc5c365033674a9c2a1b71f3a4111ad48ce6be8
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-04-24 17:23:07 -0600

    imported bootstrap

--- portfolio/src/components/UI/Navbar.jsx
+++ portfolio/src/components/UI/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar({ links }) {
     return (
       <nav className="navbar navbar-expand-lg bg-secondary">
         <div className="container-fluid">
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div  id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               {links.map((link) => link)}
             </ul>
--- portfolio/src/pages/AboutPage.jsx
+++ portfolio/src/pages/AboutPage.jsx
@@ -1,8 +1,12 @@
 export default function AboutPage() {
     return (
-        <div>
-            <img src="" alt="Itzel Hernandez Avatar"/>
-            <p></p>
+        <div className="row">
+            <div className="col-4">
+                <img src="" alt="Itzel Hernandez Avatar"/>
+            </div>
+            <div className="col-8">
+                <p>Results-driven Full-Stack Web Developer with a solid foundation in electrical engineering and multidisciplinary studies. Equipped with extensive training in a wide array of web development technologies, including Git, HTML, CSS, JavaScript, jQuery, APIs, Node.js, Express.js, Bootstrap, React, REST, MySQL/Sequelize, and MongoDB/Mongoose. Committed to delivering high-quality solutions and eager to leverage expertise in diverse technologies to drive innovation and success within dynamic development teams.</p>
+            </div>
         </div>
     );
 }
