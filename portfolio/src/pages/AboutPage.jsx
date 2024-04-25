a79a39b97835258e1e93641c40b8811b88fd596a
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-04-25 14:54:17 -0600

    edited bio

--- portfolio/src/pages/AboutPage.jsx
+++ portfolio/src/pages/AboutPage.jsx
@@ -5,7 +5,7 @@ export default function AboutPage() {
                 <img src="" alt="Itzel Hernandez Avatar"/>
             </div>
             <div className="col-8">
-                <p>Results-driven Full-Stack Web Developer with a solid foundation in electrical engineering and multidisciplinary studies. Equipped with extensive training in a wide array of web development technologies, including Git, HTML, CSS, JavaScript, jQuery, APIs, Node.js, Express.js, Bootstrap, React, REST, MySQL/Sequelize, and MongoDB/Mongoose. Committed to delivering high-quality solutions and eager to leverage expertise in diverse technologies to drive innovation and success within dynamic development teams.</p>
+                <p>Results-driven Full-Stack Web Developer with a solid foundation in electrical engineering and multidisciplinary studies. Equipped with extensive training in a wide array of web development technologies. Committed to delivering high-quality solutions and eager to leverage expertise in diverse technologies to drive innovation and success within dynamic development teams.</p>
             </div>
         </div>
     );
