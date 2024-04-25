fe79a7c1666b6f31e7c1197151d7ff93e171b748
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-04-25 15:10:07 -0600

    updated contact page with bootstrap

--- portfolio/src/pages/ContactPage.jsx
+++ portfolio/src/pages/ContactPage.jsx
@@ -1,13 +1,19 @@
 export default function ContactPage() {
     return (
         <fieldset>
-            <form>
-                <label for="name">Name:</label>
-                <input type="text" id="name"/>
-                <label for="email">Email:</label>
-                <input type="text" id="email"/>
-                <label for="message">Message:</label>
-                <textarea id="message"></textarea>
+            <form className="container-fluid">
+                <div className="row">
+                    <label for="name">Name:</label>
+                    <input type="text" id="name"/>
+                </div>
+                <div className="row">
+                    <label for="email">Email:</label>
+                    <input type="text" id="email"/>
+                </div>
+                <div className="row">
+                    <label for="message">Message:</label>
+                    <textarea id="message"></textarea>
+                </div>
                 <button type="submit" id="contact-submit">Submit</button>
             </form>
         </fieldset>
