b646eeeaaae6b0c89487ed1e66eed8548a9ccd29
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-04-25 14:29:08 -0600

    styled header with bootstrap

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -3,24 +3,30 @@ import Navbar from './UI/Navbar';
 
 export default function Header() {
     return (
-        <header>
-            <h1>Itzel Hernandez</h1>
-            <Navbar
-                links ={[
-                    <Link key={1} to="/">
-                        About Me
-                    </Link>,
-                    <Link key={2} to="/portfolio">
-                        Portfolio
-                    </Link>,
-                    <Link key={3} to="/contact">
-                        Contact
-                    </Link>,
-                    <Link key={4} to="/resume">
-                        Resume
-                    </Link>
-                ]}
-            />
+        <header className="container-fluid">
+            <div className="row">
+                <div className="col-lg">
+                    <h1>Itzel Hernandez</h1>
+                </div>
+                <div className="col-lg-auto col-md-12">
+                    <Navbar
+                        links ={[
+                            <Link key={1} to="/">
+                                About Me
+                            </Link>,
+                            <Link key={2} to="/portfolio">
+                                Portfolio
+                            </Link>,
+                            <Link key={3} to="/contact">
+                                Contact
+                            </Link>,
+                            <Link key={4} to="/resume">
+                                Resume
+                            </Link>
+                        ]}
+                    />
+                </div>
+            </div>
         </header>
     );
 }
