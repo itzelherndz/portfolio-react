7423f82204891ce85e83d06b853fd6e3968890e8
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-04-26 04:07:26 -0600

    styling

--- portfolio/src/components/UI/Navbar.jsx
+++ portfolio/src/components/UI/Navbar.jsx
@@ -1,6 +1,6 @@
 export default function Navbar({ links }) {
     return (
-      <nav className="navbar navbar-expand-lg bg-secondary">
+      <nav className="navbar navbar-expand-lg bg-secondary bg-transparent">
         <div className="container-fluid">
           <div  id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
