7df59f9007abf7862ff97a1b092d8af142a9f7c8
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-04-26 05:06:43 -0600

    added images to portfolio and descriptions

--- portfolio/src/pages/PortfolioPage.jsx
+++ portfolio/src/pages/PortfolioPage.jsx
@@ -11,12 +11,12 @@ export default function PortfolioPage() {
         <div>
             <h2>Portfolio</h2>
             <div className="row portfolio">
-                <Project image={reelReport} title="Reel Report" github="https://github.com/BryceGitHuba/Movie_Picker" deployed="https://reel-report-8887d227dacc.herokuapp.com/"/>
-                <Project image={beatTap} title="Beat Tap" github="https://github.com/gabeharvey/beat-tap" deployed="https://gabeharvey.github.io/beat-tap/"/>
-                <Project image={techBlog} title="Tech Blog" github="https://github.com/itzelherndz/tech-blog" deployed="https://tech-blog-ih-455218ad555e.herokuapp.com/"/>
-                <Project image={textEditor} title="J.A.T.E Text Editor" github="https://github.com/itzelherndz/text-editor" deployed="https://j-a-t-e-text-editor.onrender.com/"/>
-                <Project image={weatherDashboard} title="Weather Dashboard" github="https://github.com/itzelherndz/weather-dashboard" deployed="https://itzelherndz.github.io/weather-dashboard"/>
-                <Project image={codingChallenge}title="Coding Challenge" github="https://github.com/itzelherndz/js-code-quiz" deployed="https://itzelherndz.github.io/js-code-quiz/"/>
+                <Project image={reelReport} title="Reel Report" description="SQL/JS/CSS/Pico" github="https://github.com/BryceGitHuba/Movie_Picker" deployed="https://reel-report-8887d227dacc.herokuapp.com/"/>
+                <Project image={beatTap} title="Beat Tap" description="API/JS/HTML/CSS/Bulma" github="https://github.com/gabeharvey/beat-tap" deployed="https://gabeharvey.github.io/beat-tap/"/>
+                <Project image={techBlog} title="Tech Blog" description="SQL/JS/CSS/Pico" github="https://github.com/itzelherndz/tech-blog" deployed="https://tech-blog-ih-455218ad555e.herokuapp.com/"/>
+                <Project image={textEditor} title="J.A.T.E Text Editor" description="PWA"github="https://github.com/itzelherndz/text-editor" deployed="https://j-a-t-e-text-editor.onrender.com/"/>
+                <Project image={weatherDashboard} title="Weather Dashboard" description="API/JS/HTML/CSS" github="https://github.com/itzelherndz/weather-dashboard" deployed="https://itzelherndz.github.io/weather-dashboard"/>
+                <Project image={codingChallenge}title="Coding Challenge" description="JS/HTML/CSS" github="https://github.com/itzelherndz/js-code-quiz" deployed="https://itzelherndz.github.io/js-code-quiz/"/>
             </div>
         </div>     
     );
